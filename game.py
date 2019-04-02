404c998f83eb51b7a898e4bf7249aa9408da3983
Michel RASIAH <michel.rasiah@gmail.com>
2019-04-02 15:45:51 +0200

    Small web app wrapper around the Game

--- game.py
+++ game.py
@@ -6,3 +6,13 @@ class Game:
         self.grid = []
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
